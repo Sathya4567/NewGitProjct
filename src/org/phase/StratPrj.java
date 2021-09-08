f4341f6e0da84fc3523b8c5decf9dc3e35b645ab
Sathya4567 <sathyason156@gmail.com>
2021-09-08 13:14:41 +0530

    Password Functionality

--- src/org/phase/StratPrj.java
+++ src/org/phase/StratPrj.java
@@ -1,5 +1,18 @@
 package org.phase;
 
 public class StratPrj {
-
+	
+	public static void password() {
+		System.out.println("sathya priya");
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		password();
+		
+	}
+	
 }
