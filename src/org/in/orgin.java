b4939f9cb7d9cf0518ccafc93a50819d57b3d836
Sathya4567 <sathyason156@gmail.com>
2021-09-08 09:52:44 +0530

    username functionality

---
+++ src/org/in/orgin.java
@@ -0,0 +1,20 @@
+package org.in;
+
+public class orgin {
+		
+	public static void username() {
+	System.out.println("sathya");
+	}
+	public static void password() {
+		System.out.println("123");
+	
+	}
+	public static void main(String[] args) {
+		username();
+		password();
+		
+	}
+		
+	}
+	
+	
